bf2779a0c909f6aa22df8132208d6edd7c808c34
Softzino Dev <18103380@iubat.edu>
2022-12-20 19:17:26 +0600

    Practice vuejs day1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="h-full grid place-items-center" >
+  <section v-show=" inCompletedAssaignment.length ">
+    <h2 class="font-bold mb-2">In progress</h2>
+    <ul>
+      <li v-for= "assignment in inCompletedAssaignment" :key="assignment.id">
+        <label>
+          {{ assignment.name }}
+
+          <input type="checkbox" v-model="assignment.completed" >
+        </label>
+        </li>
+    </ul>
+    
+  </section>
+
+  <section v-show="completedAssaignment.length" class="mt-8">
+    <h2 class="font-bold mb-2">Complete Task</h2>
+    <ul>
+      <li 
+      v-for= "assignment in completedAssaignment"
+      :key="assignment.id"
+      >
+        <label>
+          {{ assignment.name }}
+
+          <input type="checkbox" v-model="assignment.completed" >
+        </label>
+        </li>
+    </ul>
+    
+  </section>
+  <h3>Nothing will show</h3>
+
+</div>
+ 
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App', 
+  data(){
+    return{
+      assignments :[
+        {
+          id: 1, name:"Finish Project" , completed: false
+        },
+        {
+          id: 2, name:"Read chapter 4" , completed: false
+        },
+        {
+          id: 3, name:"make a simple project" , completed: false
+        }
+      ]
+      
+    }
+  },
+  computed:{
+    completedAssaignment(){
+      return this.assignments.filter(assignment => assignment.completed)
+    },
+     inCompletedAssaignment(){
+      return this.assignments.filter(assignment => ! assignment.completed)
+    }
+
+  },
+
+
 }
 </script>
 
@@ -22,5 +79,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.notSoldOut{
+  color: green;
+}
+.soldOut{
+  color: red;
+}
+.blackTicket{
+  color: aquamarine;
 }
 </style>
